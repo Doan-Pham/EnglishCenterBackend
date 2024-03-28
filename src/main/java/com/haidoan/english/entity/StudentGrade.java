6b0903d54883c1663a9ef322c4515f0c8f13fb93
Doan Pham <doannphamm57@gmail.com>
2024-03-28 22:22:05 +0700

    Add Student Grade

---
+++ src/main/java/com/haidoan/english/entity/StudentGrade.java
@@ -0,0 +1,34 @@
+package com.haidoan.english.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "STUDENTGRADE")
+@IdClass(StudentGradeId.class)
+@Data
+public class StudentGrade {
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "StudentID")
+    private Student student;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "ClassID")
+    private Class studentClass;
+
+    @Column(name = "TestName")
+    private String testName;
+
+    @Column(name = "Grade")
+    private Float grade;
+}
+
+@Data
+class StudentGradeId implements Serializable {
+    private Long studentId;
+    private Long classId;
+}
