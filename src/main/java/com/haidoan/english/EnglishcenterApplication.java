15cabfd30a8f0870d43fa625f0537cc03563fe7b
Doan Pham <doannphamm57@gmail.com>
2024-04-15 12:04:05 +0700

    Init project

---
+++ src/main/java/com/haidoan/english/EnglishcenterApplication.java
@@ -0,0 +1,13 @@
+package com.haidoan.english;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishcenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishcenterApplication.class, args);
+	}
+
+}
